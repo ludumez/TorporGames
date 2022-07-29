3e5c72dfa60aba623f46e54e19839e75059305c1
Lukas <ludumez@hotmail.com>
2022-07-29 22:50:07 +0200

    Final commit

--- Torpor/Assets/Scripts/Codex/CategoryManager.cs
+++ Torpor/Assets/Scripts/Codex/CategoryManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CategoryManager : MonoBehaviour
 {
     public GameObject topics;
+    public TMP_Text categoryDescriptionText;
 
     public void DisableCategory() {
         topics.SetActive(false);
@@ -15,7 +17,7 @@ public class CategoryManager : MonoBehaviour
 
     public void EnableCategory() {
         topics.SetActive(true);
-
+        categoryDescriptionText.text = gameObject.name;
         //Swaps the sprites and color to the seleted ones
         GetComponent<CategorySelectionFeedback>().Seleted();
     }
