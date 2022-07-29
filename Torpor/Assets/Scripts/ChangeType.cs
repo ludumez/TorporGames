55eaed2b986e56e080da9c3d395b53b8691c3f22
Lukas <ludumez@hotmail.com>
2022-07-29 22:17:05 +0200

    Finished all funtionality
    Just need to add content

---
+++ Torpor/Assets/Scripts/ChangeType.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeType : MonoBehaviour
+{
+    public GameObject codex;
+    public GameObject notes;
+    GameObject currentObject;
+
+    private void Start()
+    {
+        currentObject = codex;
+    }
+
+    public void EnableObject(GameObject type) {
+        currentObject.SetActive(false);
+        currentObject = type;
+        type.SetActive(true);
+    }
+}
